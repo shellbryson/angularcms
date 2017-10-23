7cf674090bc7e7ca3e1476f9683d763e11fc4599
Shell Bryson <shell@shellbryson.com>
2017-10-23 11:14:15 +0100

    Fetch pages, page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { PageService } from './services/page.service';
+import { PagesComponent } from './components/pages/pages.component';
+
+const appRoutes: Routes = [
+  { path: ':page', component: PagesComponent},
+  { path: '', component: PagesComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FormsModule
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     PageService
--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -1,5 +1,6 @@
 import { Http } from '@angular/http';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
 
 import { Injectable } from '@angular/core';
 
@@ -9,5 +10,10 @@ export class PageService {
   constructor(private http: Http) { }
 
   public pagesBS = new BehaviorSubject<string>(null);
+
+  getPages() {
+    return this.http.get('http://localhost:3000/pages')
+      .map(res => res.json());
+  }
 
 }
